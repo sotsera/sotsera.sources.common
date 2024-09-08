dd6ffc58ac2a1e83cac9ed394425ac30faf1a8a8
Alessandro Ghidini <ghidello@users.noreply.github.com>
2024-09-08 10:45:25 +0200

    initial solution

---
+++ test/Sotsera.Sources.Common.Tests.Unit/Extensions/ObjectExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using Sotsera.Sources.Common.Extensions;
+
+namespace Sotsera.Sources.Common.Tests.Unit.Extensions;
+
+public class ObjectExtensionsTests
+{
+    [Fact]
+    public void ThrowIfNull_ShouldThrowArgumentNullException_WhenValueIsNull()
+    {
+        object? value = null;
+
+        Action act = () => value.ThrowIfNull();
+
+        act.Should().Throw<ArgumentNullException>().WithParameterName(nameof(value));
+    }
+
+
+    [Theory]
+    [InlineData("ciao")]
+    [InlineData(true)]
+    public void ThrowIfNull_ShouldNotThrowArgumentNullException_WhenValueIsNotNull(object? value)
+    {
+        Action act = () => value.ThrowIfNull();
+
+        act.Should().NotThrow();
+    }
+}
